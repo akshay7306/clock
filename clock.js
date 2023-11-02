f3e35f50bf75b3bfd5f766857ca5215eed4efbe7
akshay730699 <akshay730699@gmail.com>
2023-11-02 08:59:01 +0530

    clock

---
+++ clock.js
@@ -0,0 +1,18 @@
+function clockTime(){
+    // to getsystem
+
+    let date=new Date()
+    let hours=date.getHours()
+    let minute=date.getMinutes()
+    let second=date.getSeconds()
+
+    clock.innerHTML=`${hours}:${minute}:${second}`
+
+    setTimeout(() =>{
+        clockTime()
+    }, 1000);
+}
+
+// function call
+
+clockTime()
